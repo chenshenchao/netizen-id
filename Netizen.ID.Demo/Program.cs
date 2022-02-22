27e39bd1c7a26dab99e1080f165c23e6504b731c
chenshenchao <chenshenchao@outlook.com>
2022-02-22 21:33:34 +0800

    make many

--- Netizen.ID.Demo/Program.cs
+++ Netizen.ID.Demo/Program.cs
@@ -6,10 +6,21 @@ namespace Netizen.ID.Demo
     {
         static void Main(string[] args)
         {
-            IdentifierMaker idmkr = new IdentifierMaker(0);
+            IdentifierMaker idmkr = new IdentifierMaker();
+            long[] ids1 = new long[1000];
             for (int i = 0; i <= 1000; ++i)
             {
-                Console.WriteLine(idmkr.Make());
+                ids1[i] = idmkr.Make();
+            }
+            foreach(long id1 in ids1)
+            {
+                Console.WriteLine(id1);
+            }
+
+            long[] ids2 = idmkr.MakeMany(1000);
+            foreach (long id2 in ids2)
+            {
+                Console.WriteLine(id2);
             }
             Console.ReadLine();
         }
--- Netizen.ID/IdentifierMaker.cs
+++ Netizen.ID/IdentifierMaker.cs
@@ -2,22 +2,38 @@
 
 namespace Netizen.ID
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class IdentifierMaker
     {
+        public const int TimestampLength = 41;
+        public const int IdLength = 12;
+        public const int MakerIdLength = 10;
+        public const long IdMax = (1 << IdLength) - 1;
+
         public long MakerId { get; private set; }
         public long LastId { get; private set; }
         public long LastAt { get; private set; }
 
-        public IdentifierMaker(long makerId)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="makerId"></param>
+        public IdentifierMaker(long makerId=0)
         {
             MakerId = makerId;
-            LastId = 0;
+            LastId = 1;
             LastAt = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         public long Make()
         {
-            long mid = MakerId << (41 + 12);
+            long mid = MakerId << (TimestampLength + IdLength);
             long now = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
             if (LastAt == now)
             {
@@ -25,11 +41,42 @@ namespace Netizen.ID
             }
             else
             {
-                LastId = 0;
+                LastId = 1;
                 LastAt = now;
             }
 
-            return mid | (LastAt << 12) | LastId;
+            return mid | (LastAt << IdLength) | LastId;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="count"></param>
+        /// <returns></returns>
+        public long[] MakeMany(long count)
+        {
+            long[] result = new long[count];
+            long mid = MakerId << (TimestampLength + IdLength);
+            long now = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+            if (LastAt == now)
+            {
+                ++LastAt;
+            }
+            LastId = 0;
+            for (int i = 0; i < count; i++)
+            {
+                if (LastId < IdMax)
+                {
+                    ++LastId;
+                }
+                else
+                {
+                    LastId = 0;
+                    ++LastAt;
+                }
+                result[i] = mid | (LastAt << IdLength) | LastId;
+            }
+            return result;
         }
     }
 }
